4af9ab06ca2d23c6fc205171fa3ea290a4790077
sanv1zl <sanva970@gmail.com>
2024-04-11 00:37:28 +0300

    bit of style to tasks

--- src/tasks/Tasks.js
+++ src/tasks/Tasks.js
@@ -2,7 +2,17 @@ import TasksList from "./TasksList"
 
 export default function Tasks() {
     return (<>
-    <h1>Tasks</h1>
-    <TasksList />
+    <ul className="task-ul">
+        <li>
+            <div className="tasks-left">
+                <h1>Options</h1>
+            </div>
+        </li>
+        <li><div className="tasks-right">
+                <h1 className="task-header">Tasks</h1>
+                <TasksList />
+            </div>
+        </li>
+    </ul>
     </>)
 }
